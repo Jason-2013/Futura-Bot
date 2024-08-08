7c45e680598e090f6484739f4d9113d561e3bf03
Jason-2013 <keeven_zp@163.com>
2024-08-08 17:50:29 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = "Basic Sphinx Example Project"
-copyright = "2022, Read the Docs core team"
-author = "Read the Docs core team"
+project = "Futura Bot Project"
+copyright = "2024, Futura bot core team"
+author = "Futura bot core team"
 
 
 # -- General configuration ---------------------------------------------------
